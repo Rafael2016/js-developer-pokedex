8ba3cca97f81daa08a3cd85d72948464df48ef8b
Rafael Luz <rafa.tecnologo@hotmail.com>
2023-09-21 00:12:21 -0300

    - Desafio Pokédex Modal @Rafael Luz

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
+
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
+const pokemonDetail = document.getElementById('pokemonDetail')
+
 
 const maxRecords = 151
-const limit = 10
+const limit = 12
 let offset = 0;
 
 function convertPokemonToLi(pokemon) {
@@ -15,11 +18,30 @@ function convertPokemonToLi(pokemon) {
                 <ol class="types">
                     ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
-
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <button class="pokemon ${pokemon.type} button-detail" data-modal ="${pokemon.name}" title="Click to see more details" onclick="openModal(this)">
+                <span class="text-detail">Show more detail</span>
+            </button>
         </li>
+        <div id="modal_${pokemon.name}" class="modal">
+            <li class="detail-pokemon">
+            <span class="fechar" data-modal ="${pokemon.name}" onclick="closedModal(this)" >&times;</span>
+                <div class="">
+                    <h4 style="color:rgba(0, 0, 0, 0.7);">Abilites</h4>
+                    <ol ol-detail>
+                        ${pokemon.abilities.map((ability) => `<li class=" ${ability}">${ability}</li>`).join('')}
+                    </ol>
+                </div>
+                <div class="">
+                    <h4 style="color:rgba(0, 0, 0, 0.7);">Attributes</h4>
+                    <ol class="">
+                        ${pokemon.stats.map((stat) => `<li class=" ${stat}">${stat}</li>`).join('')}
+                    </ol>
+                </div>
+            </li>    
+        </div>
     `
 }
 
@@ -29,8 +51,6 @@ function loadPokemonItens(offset, limit) {
         pokemonList.innerHTML += newHtml
     })
 }
-
-loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
@@ -45,3 +65,24 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+loadPokemonItens(offset, limit)
+
+/**
+ * @OPEN MODAL
+ */
+openModal=(self)=>{
+
+    const element =  self.getAttribute("data-modal")   
+    document.getElementById("modal_"+element).style.display = "block"
+
+}
+/**
+ * @CLOSED MODAL
+ */
+closedModal=(self)=>{
+
+    const element =  self.getAttribute("data-modal")   
+    document.getElementById("modal_"+element).style.display = "none"
+
+}
